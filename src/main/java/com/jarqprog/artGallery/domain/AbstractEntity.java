6264b7901f90aa88f7e7cf356a8d62b6715f932e
jarqprog <jarqprog@gmail.com>
2019-08-03 02:59:41 +0200

    add auditing

--- src/main/java/com/jarqprog/artGallery/domain/AbstractEntity.java
+++ src/main/java/com/jarqprog/artGallery/domain/AbstractEntity.java
@@ -3,30 +3,42 @@ package com.jarqprog.artGallery.domain;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.annotation.Version;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Setter
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 abstract class AbstractEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @CreatedDate
+    @GeneratedValue
     private LocalDateTime createdDate;
 
     @LastModifiedDate
+    @GeneratedValue
     private LocalDateTime modifiedDate;
 
     @Version
+    @GeneratedValue
     private Long version;
+
+//    @CreatedBy
+//    private User user;
+
+//    @LastModifiedBy
+//    private User user;
+
+    private LocalDateTime discontinueDate;
 
 }
--- src/main/java/com/jarqprog/artGallery/repository/PictureRepository.java
+++ src/main/java/com/jarqprog/artGallery/repository/PictureRepository.java
@@ -2,12 +2,17 @@ package com.jarqprog.artGallery.repository;
 
 import com.jarqprog.artGallery.domain.Picture;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@EnableTransactionManagement
 public interface PictureRepository extends JpaRepository<Picture, Long> {
 
     Optional<Picture> findById(Long id);
@@ -15,5 +20,9 @@ public interface PictureRepository extends JpaRepository<Picture, Long> {
     List<Picture> findTop5ByTitle(String title);
     List<Picture> findByTitleIsNull();
     List<Picture> findAll();
+
+    @Modifying(clearAutomatically = true)
+    @Query("update Picture p set p.discontinueDate = getDate() where p.id =:pictureId")
+    void deleteById(@Param("pictureId") Long pictureId);
 
 }
