70529475abaae237753d7ea46ffbaad3efbbb31d
jarqprog <jarqprog@gmail.com>
2019-08-04 21:42:19 +0200

    add contact service implementation

--- src/main/java/com/jarqprog/artGallery/service/contact/SimpleContactService.java
+++ src/main/java/com/jarqprog/artGallery/service/contact/SimpleContactService.java
@@ -1,36 +1,65 @@
 package com.jarqprog.artGallery.service.contact;
 
+import com.jarqprog.artGallery.domain.Contact;
 import com.jarqprog.artGallery.dto.ContactDTO;
+import com.jarqprog.artGallery.helper.DtoEntityConverter;
 import com.jarqprog.artGallery.repository.ContactRepository;
+import com.jarqprog.artGallery.service.metadata.EntityMetadataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class SimpleContactService implements ContactService {
 
-    @Autowired
-    private ContactRepository contactRepository;
+    @Autowired private ContactRepository contactRepository;
+
+    @Autowired private DtoEntityConverter dtoEntityConverter;
+
+    @Autowired private EntityMetadataService entityMetadataService;
 
     @Override
     public List<ContactDTO> getAllContacts() {
-        return null;
+        return contactRepository.findAll()
+                .stream()
+                .map(c -> dtoEntityConverter.convertEntityToDto(c, ContactDTO.class))
+                .collect(Collectors.toList());
     }
 
     @Override
     public ContactDTO findById(long id) throws EntityNotFoundException {
-        return null;
+        Contact contact = findContactById(id);
+        return dtoEntityConverter.convertEntityToDto(contact, ContactDTO.class);
     }
 
     @Override
     public ContactDTO save(ContactDTO contactDTO) {
-        return null;
+        Contact contact = dtoEntityConverter.convertDtoToEntity(contactDTO, Contact.class);
+        Contact saved = contactRepository.save(contact);
+        entityMetadataService.createMetadata(saved);
+        return dtoEntityConverter.convertEntityToDto(saved, ContactDTO.class);
     }
 
     @Override
     public boolean remove(long id) {
-        return false;
+        boolean isRemoved = false;
+        try {
+            Contact contact = findContactById(id);
+            entityMetadataService.markDiscontinued(contact);
+            contactRepository.delete(contact);
+            isRemoved = true;
+        } catch (EntityNotFoundException e) {
+            //todo
+        }
+        return isRemoved;
+    }
+
+    private Contact findContactById(Long id) throws EntityNotFoundException {
+        return contactRepository
+                .findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(String.valueOf(id)));
     }
 }
