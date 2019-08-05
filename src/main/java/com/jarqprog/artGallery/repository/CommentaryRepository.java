a0fabb5a9191a45386d4bd7f3d7ab47a2f7df5c6
jarqprog <jarqprog@gmail.com>
2019-08-05 15:22:09 +0200

    fix delete issue in commentary repository

--- src/main/java/com/jarqprog/artGallery/repository/CommentaryRepository.java
+++ src/main/java/com/jarqprog/artGallery/repository/CommentaryRepository.java
@@ -2,6 +2,8 @@ package com.jarqprog.artGallery.repository;
 
 import com.jarqprog.artGallery.domain.Commentary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -13,4 +15,8 @@ public interface CommentaryRepository extends JpaRepository<Commentary, Long>  {
 
     List<Commentary> findAllCommentaryByPictureId(long pictureId);
 
+    @Override
+    @Modifying
+    @Query("delete from Commentary c where c.id = ?1")
+    void deleteById(Long id);
 }
