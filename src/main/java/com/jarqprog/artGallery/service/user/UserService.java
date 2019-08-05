a759d1146bcdd47b30e6e3b158e626a36e98f0ad
jarqprog <jarqprog@gmail.com>
2019-08-05 23:03:34 +0200

    remove methods from user interface, add logger

--- src/main/java/com/jarqprog/artGallery/service/user/UserService.java
+++ src/main/java/com/jarqprog/artGallery/service/user/UserService.java
@@ -1,16 +1,10 @@
 package com.jarqprog.artGallery.service.user;
 
-import com.jarqprog.artGallery.domain.User;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public interface UserService {
 
-    List<User> getAllUsers();
-    <P extends User> P save(P user);
-    Optional<User> findById(Long id);
-    boolean remove(Long id);
+
+
 }
