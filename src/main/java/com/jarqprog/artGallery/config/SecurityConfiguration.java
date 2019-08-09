d6195b87b359e076cfe9241a163316d74501b3d5
jarqprog <jarqprog@gmail.com>
2019-08-09 18:38:15 +0200

    add empty class securityConfiguration

--- src/main/java/com/jarqprog/artGallery/config/SecurityConfiguration.java
+++ src/main/java/com/jarqprog/artGallery/config/SecurityConfiguration.java
@@ -1,31 +1,61 @@
 package com.jarqprog.artGallery.config;
 
 
-import com.jarqprog.artGallery.repository.UserRepository;
+import com.jarqprog.artGallery.domain.Roles;
+import com.jarqprog.artGallery.service.user.SimpleUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import javax.naming.AuthenticationException;
+
 
 //@Configuration
 //@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 //@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+
+//    @Autowired private SimpleUserDetailsService userDetailsService;
+//    @Autowired private AuthenticationProvider authenticationProvider;
 //
-//    @Autowired private UserDetailsService userDetailsService;
+//    @Bean
+//    public AuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+//        provider.setUserDetailsService(userDetailsService);
+//        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+//        return provider;
+//    }
 //
 //    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-
-
-
-
+//    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+//        authenticationManagerBuilder
+//            .authenticationProvider(authenticationProvider)
+//            .userDetailsService(userDetailsService)
+//            .passwordEncoder(new BCryptPasswordEncoder());
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+////            .cors().and().csrf().disable()
+//            .authorizeRequests()
+//            .antMatchers("/pictures/**").permitAll()  // request matcher for anonymous user
+//            .antMatchers("/contacts/**").hasRole(Roles.USER.name()) // check for authority with ROLE_USER in the database
+//            .antMatchers("/users/**").hasRole(Roles.ADMIN.name()) // check for authority with ROLE_ADMIN in the database.authenticated().and().exceptionHandling()
+//            .anyRequest().authenticated()
+////            .and().formLogin()
+//            .and().httpBasic();
+////            .and().sessionManagement()
+////            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 //    }
 }
