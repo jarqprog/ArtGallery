48d02fac6e32fc9c26d579ee911001f21333fdde
jarqprog <jarqprog@gmail.com>
2019-08-09 18:36:58 +0200

    add overloaded constructor to resource not found exception

--- src/main/java/com/jarqprog/artGallery/exception/persistenceException/ResourceNotFoundException.java
+++ src/main/java/com/jarqprog/artGallery/exception/persistenceException/ResourceNotFoundException.java
@@ -9,4 +9,8 @@ public class ResourceNotFoundException extends EntityNotFoundException {
     public ResourceNotFoundException(Class<? extends DomainEntity> clazz, long entityId) {
         super(String.format("Resource %s with ID=%s not found", clazz.getSimpleName(), entityId));
     }
+
+    public ResourceNotFoundException(Class<? extends DomainEntity> clazz) {
+        super(String.format("Resource %s not found", clazz.getSimpleName()));
+    }
 }
