c701aeb55644473658c339e07b151b83f508287c
jarqprog <jarqprog@gmail.com>
2019-08-16 02:48:10 +0200

    add method findOneByEmail

--- src/main/java/com/jarqprog/artGallery/springData/repository/ContactRepository.java
+++ src/main/java/com/jarqprog/artGallery/springData/repository/ContactRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.util.Optional;
+
 @Repository
 @EnableTransactionManagement
 public interface ContactRepository extends JpaRepository<Contact, Long> {
 
-
+    Optional<Contact> findOneByEmail(String email);
 
 }
