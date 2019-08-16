3ba3ed11912e2332a0416a56b8e584178bd499f1
jarqprog <jarqprog@gmail.com>
2019-08-16 02:39:55 +0200

    add method findOneByLogin

--- src/main/java/com/jarqprog/artGallery/springData/repository/UserRepository.java
+++ src/main/java/com/jarqprog/artGallery/springData/repository/UserRepository.java
@@ -12,5 +12,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findUserByLogin(String login);
+    Optional<User> findOneByLogin(String login);
     Optional<User> findUserByContactId(long contactId);
 }
