769a08cb8d53dddb26693e808a6f1e25b881cfee
jarqprog <jarqprog@gmail.com>
2019-10-18 11:55:26 +0200

    add simple test method

--- src/test/java/com/jarqprog/artGallery/springData/services/commentaries/RetrievingAllCommentariesTest.java
+++ src/test/java/com/jarqprog/artGallery/springData/services/commentaries/RetrievingAllCommentariesTest.java
@@ -1,28 +1,44 @@
 package com.jarqprog.artGallery.springData.services.commentaries;
 
 import com.jarqprog.artGallery.SpringServiceTestConfig;
+import com.jarqprog.artGallery.domain.dto.CommentaryDTO;
 import com.jarqprog.artGallery.domain.useCases.CommentaryService;
-import org.apache.log4j.Logger;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
 
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = SpringServiceTestConfig.class)
 class RetrievingAllCommentariesTest {
 
-    private static final Logger logger = Logger.getLogger(RetrievingAllCommentariesTest.class);
+    private static final Logger logger = LoggerFactory.getLogger(RetrievingAllCommentariesTest.class);
+
+    private final CommentaryService commentaryService;
 
     @Autowired
-    private CommentaryService commentaryService;
+    public RetrievingAllCommentariesTest(CommentaryService commentaryService) {
+        this.commentaryService = commentaryService;
+    }
 
     @Test
-    void findCommentaryById() {
-        Object object = commentaryService.findCommentaryById(1L);
-        logger.info(String.valueOf(object));
+    @DisplayName("should retrieve 4 commentaries")
+    void checkQuantityIsCorrect() {
+
+        int expected = 4;
+
+        List<CommentaryDTO> commentaries = commentaryService.getAllCommentaries();
+
+        assertEquals(expected, commentaries.size());
     }
 
     @Test
