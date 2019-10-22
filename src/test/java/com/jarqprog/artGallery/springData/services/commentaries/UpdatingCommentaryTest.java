a2297b27b431c6520d95048c642e154b0195a91e
jarqprog <jarqprog@gmail.com>
2019-10-22 11:05:06 +0200

    add new tests

---
+++ src/test/java/com/jarqprog/artGallery/springData/services/commentaries/UpdatingCommentaryTest.java
@@ -0,0 +1,106 @@
+package com.jarqprog.artGallery.springData.services.commentaries;
+
+
+import com.jarqprog.artGallery.SpringServiceTestConfig;
+import com.jarqprog.artGallery.domain.dto.CommentaryDTO;
+import com.jarqprog.artGallery.domain.dto.PictureDTO;
+import com.jarqprog.artGallery.domain.dto.UserDTO;
+import com.jarqprog.artGallery.domain.useCases.CommentaryService;
+import com.jarqprog.artGallery.springData.exceptions.ResourceNotFoundException;
+import com.jarqprog.artGallery.springData.exceptions.InvalidObjectException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Values used in test methods are related to values set in InitialDataLoader
+ */
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = SpringServiceTestConfig.class)
+@Rollback
+@Transactional
+class UpdatingCommentaryTest {
+
+    private final CommentaryService commentaryService;
+
+    @Autowired
+    public UpdatingCommentaryTest(CommentaryService commentaryService) {
+        assert commentaryService != null;
+        this.commentaryService = commentaryService;
+    }
+
+    @Test
+    @DisplayName("ResourceNotFoundException should be thrown")
+    void tryToUpdateNotExistingCommentary() {
+
+        long notExistingCommentaryId = 76576;
+
+        CommentaryDTO existingCommentary = commentaryService.findCommentaryById(1);
+        PictureDTO pictureDTO = existingCommentary.getPicture();
+        long pictureId = pictureDTO.getId();
+
+        CommentaryDTO invalidCommentary = new CommentaryDTO();
+        invalidCommentary.setId(notExistingCommentaryId);
+        invalidCommentary.setComment("I am not in DB");
+        invalidCommentary.setPicture(pictureDTO);
+        invalidCommentary.setUser(existingCommentary.getUser());
+
+        assertThrows(ResourceNotFoundException.class,
+                () -> commentaryService.updateCommentary(pictureId, notExistingCommentaryId, invalidCommentary));
+    }
+
+    @Test
+    @DisplayName("ResourceNotFoundException should be thrown")
+    void tryToUpdateExistingCommentaryUsingInvalidPictureId() {
+
+        long notExistingPictureId = 76576;
+
+        CommentaryDTO existingCommentary = commentaryService.findCommentaryById(1);
+        existingCommentary.setComment("changed");
+
+        long commentaryId = existingCommentary.getId();
+
+        assertThrows(ResourceNotFoundException.class,
+                () -> commentaryService.updateCommentary(notExistingPictureId, commentaryId, existingCommentary));
+    }
+
+    @Test
+    @DisplayName("InvalidObjectException should be thrown")
+    void tryToUpdateExistingCommentaryWithoutUser() {
+
+        CommentaryDTO existingCommentary = commentaryService.findCommentaryById(1);
+        PictureDTO pictureDTO = existingCommentary.getPicture();
+        long pictureId = pictureDTO.getId();
+        long commentaryId = existingCommentary.getId();
+
+        existingCommentary.setUser(null);
+
+        assertThrows(InvalidObjectException.class,
+                () -> commentaryService.updateCommentary(pictureId, commentaryId, existingCommentary));
+    }
+
+    @Test
+    @DisplayName("InvalidObjectException should be thrown - user has invalid Id")
+    void tryToUpdateExistingCommentaryWithInvalidUser() {
+
+        CommentaryDTO existingCommentary = commentaryService.findCommentaryById(1);
+        PictureDTO pictureDTO = existingCommentary.getPicture();
+        UserDTO userDTO = existingCommentary.getUser();
+        long pictureId = pictureDTO.getId();
+        long commentaryId = existingCommentary.getId();
+
+        userDTO.setId(-10);
+
+        assertThrows(InvalidObjectException.class,
+                () -> commentaryService.updateCommentary(pictureId, commentaryId, existingCommentary));
+    }
+
+
+}
