87dcbdcaaae91e0916e905b2e4372340816a7c93
jarqprog <jarqprog@gmail.com>
2019-10-23 22:13:49 +0200

    add marker interfaces

---
+++ src/main/java/com/jarqprog/artGallery/domain/dto/heavyDto/HeavyDTO.java
@@ -0,0 +1,10 @@
+package com.jarqprog.artGallery.domain.dto.heavyDto;
+
+import java.io.Serializable;
+
+/**
+ * Heavy DTOs have nested DTOs objects (not only their IDs)
+ */
+public interface HeavyDTO extends Serializable {
+    //marker interface
+}
---
+++ src/main/java/com/jarqprog/artGallery/domain/dto/lightDto/LightDTO.java
@@ -0,0 +1,11 @@
+package com.jarqprog.artGallery.domain.dto.lightDto;
+
+
+import java.io.Serializable;
+
+/**
+ * Light DTOs do not have nested DTOs objects, only their IDs
+ */
+public interface LightDTO  extends Serializable {
+    // marker interface
+}
