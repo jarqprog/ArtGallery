bf53337c351131edfcd3ed7f9364cb1674d0ef9f
jarqprog <jarqprog@gmail.com>
2019-10-23 17:07:56 +0200

    refactor

--- src/main/java/com/jarqprog/artGallery/springWebMVC/controller/ErrorController.java
+++ src/main/java/com/jarqprog/artGallery/springWebMVC/controller/ErrorController.java
@@ -1,21 +1,15 @@
 package com.jarqprog.artGallery.springWebMVC.controller;
 
 import com.jarqprog.artGallery.springData.exceptions.HttpExceptionInfo;
-import com.jarqprog.artGallery.springData.exceptions.ResourceNotFoundException;
 import com.jarqprog.artGallery.springData.exceptions.implementation.HttpExceptionInfoImpl;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @ControllerAdvice
 public class ErrorController extends ResponseEntityExceptionHandler {
