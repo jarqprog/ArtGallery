2136951fc3e3bbba3154e7a6c05b9e0f921f736c
jarqprog <jarqprog@gmail.com>
2019-10-23 17:02:43 +0200

    add better exception handling in controller layer

--- src/main/java/com/jarqprog/artGallery/springData/exceptions/HttpExceptionInfo.java
+++ src/main/java/com/jarqprog/artGallery/springData/exceptions/HttpExceptionInfo.java
@@ -1,11 +1,13 @@
 package com.jarqprog.artGallery.springData.exceptions;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public interface ExceptionInfo {
+public interface HttpExceptionInfo extends Serializable {
 
     String getUuid();
     long getHttpStatus();
     LocalDateTime getDateTime();
     String getMessage();
+    String getExceptionInfo();
 }
--- src/main/java/com/jarqprog/artGallery/springWebMVC/controller/ErrorController.java
+++ src/main/java/com/jarqprog/artGallery/springWebMVC/controller/ErrorController.java
@@ -1,36 +1,51 @@
 package com.jarqprog.artGallery.springWebMVC.controller;
 
-import com.jarqprog.artGallery.springData.exceptions.ExceptionInfo;
-import com.jarqprog.artGallery.springData.exceptions.implementation.ExceptionInfoImpl;
+import com.jarqprog.artGallery.springData.exceptions.HttpExceptionInfo;
+import com.jarqprog.artGallery.springData.exceptions.ResourceNotFoundException;
+import com.jarqprog.artGallery.springData.exceptions.implementation.HttpExceptionInfoImpl;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @ControllerAdvice
 public class ErrorController extends ResponseEntityExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(ErrorController.class);
 
-    //todo: need to recognize failures on client side and inform
+    //todo: need to implement this for varied exception and HttpStatuses - at the moment it is general solution
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    protected String handleConflict(final Exception exception, final Model model){
-
-        ExceptionInfo clientExceptionInfo = ExceptionInfoImpl
-                .getClientInfo(HttpStatus.INTERNAL_SERVER_ERROR.value(), exception);
-        model.addAttribute("message", clientExceptionInfo.getMessage());
-        model.addAttribute("errorId", clientExceptionInfo.getUuid());
-        model.addAttribute("date", clientExceptionInfo.getDateTime());
-        model.addAttribute("status", clientExceptionInfo.getHttpStatus());
-
-        ExceptionInfo logExceptionInfo = ExceptionInfoImpl.getLogInfo(clientExceptionInfo, exception);
-        logger.warn(logExceptionInfo.toString());
+    protected String handleConflict(final Exception exception, final Model model) {
+        handleExceptionInfo("Something gone wrong", exception, model, HttpStatus.INTERNAL_SERVER_ERROR);
         return "/error/error";
     }
+
+    private void handleExceptionInfo(String message, final Exception exception,
+                                     final Model model, HttpStatus httpStatus) {
+
+        HttpExceptionInfo httpExceptionInfo = new HttpExceptionInfoImpl(httpStatus, message, exception);
+        model.addAttribute("clientInfo", httpExceptionInfo);
+        logger.error(String.format("UUID: %s - Exception in controller layer", httpExceptionInfo.getUuid()), exception);
+    }
+
+    //use this one for REST
+//    @ExceptionHandler(ResourceNotFoundException.class)
+//    public ResponseEntity<HttpExceptionInfo> customHandleNotFound(Exception ex, WebRequest request) {
+//        HttpExceptionInfo httpExceptionInfo = new HttpExceptionInfoImpl(httpStatus, message, exception);
+//        model.addAttribute("exceptionInfo", httpExceptionInfo);
+//        logger.error(String.format("UUID: %s - Exception in controller layer", httpExceptionInfo.getUuid()), exception);
+//
+//        return new ResponseEntity<>(exceptionInfo, HttpStatus.NOT_FOUND);
+//    }
 }
