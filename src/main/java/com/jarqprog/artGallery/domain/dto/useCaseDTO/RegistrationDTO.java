0cf4d0f1d6c6e3423c4952826725bc5abbda258f
jarqprog <jarqprog@gmail.com>
2019-10-24 10:06:13 +0200

    remove validation annotation from registration DTO

--- src/main/java/com/jarqprog/artGallery/domain/dto/useCaseDTO/RegistrationDTO.java
+++ src/main/java/com/jarqprog/artGallery/domain/dto/useCaseDTO/RegistrationDTO.java
@@ -2,20 +2,13 @@ package com.jarqprog.artGallery.domain.dto.useCaseDTO;
 
 import lombok.Data;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 @Data
 public class RegistrationDTO {
 
-    private static final int MIN_LOGIN_LENGTH = 4;
-    private static final int MIN_PASSWORD_LENGTH = 4;
-
-    @NotNull private String firstName;
+    private String firstName;
     private String lastName;
     private String nickname;
-    @NotNull @Email private String email;
-    @NotNull @Size(min=MIN_LOGIN_LENGTH) private String login;
-    @NotNull @Size(min=MIN_PASSWORD_LENGTH) private String password;
+    private String email;
+    private String login;
+    private String password;
 }
