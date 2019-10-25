7d865ad90d84eecea156bf4aaff5a0f7041f671c
jarqprog <jarqprog@gmail.com>
2019-10-25 11:38:32 +0200

    add OutputMode for Rest controllers

---
+++ src/main/java/com/jarqprog/artGallery/api/rest/controller/OutputMode.java
@@ -0,0 +1,8 @@
+package com.jarqprog.artGallery.api.rest.controller;
+
+public interface OutputMode {
+
+    String FAT = "fat";
+    String THIN = "thin";
+    // todo add HATEOAS when implemented
+}
