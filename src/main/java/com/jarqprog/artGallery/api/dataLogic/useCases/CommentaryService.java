e7da565093a1b8ebc96dbba7bb3b45d8dfcd282f
jarqprog <jarqprog@gmail.com>
2019-10-25 11:47:50 +0200

    refactor

--- src/main/java/com/jarqprog/artGallery/api/dataLogic/useCases/CommentaryService.java
+++ src/main/java/com/jarqprog/artGallery/api/dataLogic/useCases/CommentaryService.java
@@ -12,11 +12,16 @@ public interface CommentaryService {
 
     List<CommentaryDTO> getAllCommentaries();
     <T extends CommentaryDTO> List<T> getAllCommentaries(Class<T> clazz);
+
     List<CommentaryDTO> getAllCommentariesByPicture(long pictureId);
     <T extends CommentaryDTO> List<T> getAllCommentariesByPicture(long pictureId, Class<T> clazz);
+
     CommentaryDTO findCommentaryById(long id);
     <T extends CommentaryDTO> T findCommentaryById(long id, Class<T> clazz);
+
     CommentaryDTO addCommentary(long pictureId, @NonNull CommentaryDTO commentaryDTO);
+
     CommentaryDTO updateCommentary(long pictureId, long commentaryId, @NonNull CommentaryDTO commentaryDTO);
+
     void removeCommentary(long id);
 }
