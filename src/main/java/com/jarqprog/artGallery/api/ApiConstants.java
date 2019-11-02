750d9e7abad0ff7425d16cad784c62f46c7640fa
jarqprog <jarqprog@gmail.com>
2019-11-02 08:38:15 +0100

    add spring profiles to api constants

--- src/main/java/com/jarqprog/artGallery/api/ApiConstants.java
+++ src/main/java/com/jarqprog/artGallery/api/ApiConstants.java
@@ -4,4 +4,10 @@ public interface ApiConstants {
 
     String VERSION = "v1";
     String BASE_URL_PATH = "/api/" + VERSION + "/";
+
+
+    //profiles
+    String DEV_PROFILE = "dev";
+    String PROD_PROFILE = "prod";
+    String TEST_PROFILE = "test";
 }
