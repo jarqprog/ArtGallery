38691d7180f17499205bd3f2674ab8c91298a09b
jarqprog <jaroslaw.kucharczyk@tesco.com>
2019-11-21 08:36:13 +0100

    add DummyUserService to UserWebController

--- codebase/web/src/main/java/com/jarqprog/web/controller/UserWebController.java
+++ codebase/web/src/main/java/com/jarqprog/web/controller/UserWebController.java
@@ -2,10 +2,13 @@ package com.jarqprog.web.controller;
 
 
 import com.jarqprog.domainperson.model.contact.ContactData;
-import com.jarqprog.web.AppInitializer;
+import com.jarqprog.web.service.DummyUserService;
 import lombok.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,19 +21,20 @@ public class UserWebController {
 
     private static final Logger logger = LoggerFactory.getLogger(UserWebController.class);
 
-//    @NonNull private final UserService userService;
-//
-//    @Autowired
-//    public UserWebController(@NonNull UserService userService) {
-//        this.userService = userService;
-//    }
+    @NonNull private final DummyUserService userService;
+
+    @Autowired
+    public UserWebController(@NonNull DummyUserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/user-info")
     public String showUserInfo(@NonNull Model model) {
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        String login = authentication.getName();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String login = authentication.getName();
+
 //        UserFat user = userService.findUserByLogin(login, UserFat.class);
-        var user = AppInitializer.getDummyUser();
+        var user = userService.getDummyUser();
         ContactData contactData = user.getContact();
         model.addAttribute("login", user.getLogin());
         model.addAttribute("email", contactData.getEmail());
