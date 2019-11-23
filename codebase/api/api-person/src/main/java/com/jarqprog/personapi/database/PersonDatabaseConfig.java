355ba4879f70ffb16e38477b3fa697456e817652
jarqprog <jaroslaw.kucharczyk@tesco.com>
2019-11-23 20:11:42 +0100

    remove component annotation

--- codebase/api/api-person/src/main/java/com/jarqprog/personapi/database/PersonDatabaseConfig.java
+++ codebase/api/api-person/src/main/java/com/jarqprog/personapi/database/PersonDatabaseConfig.java
@@ -1,9 +1,7 @@
 package com.jarqprog.personapi.database;
 
 import com.jarqprog.commonapi.DatabaseConfig;
-import org.springframework.stereotype.Component;
 
-@Component
 public interface PersonDatabaseConfig extends DatabaseConfig {
 
     String DATABASE_NAME = "person_db";
