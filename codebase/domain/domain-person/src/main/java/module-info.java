0858207f46224b556f6ffa1c5360b33d0412cf37
jarqprog <jaroslaw.kucharczyk@tesco.com>
2019-11-24 09:40:37 +0100

    rename package

--- codebase/domain/domain-person/src/main/java/module-info.java
+++ codebase/domain/domain-person/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module domain.person {
     exports com.jarqprog.domainperson.model.contact;
-    exports com.jarqprog.domainperson.model.userrole;
+    exports com.jarqprog.domainperson.model.roleuser;
     exports com.jarqprog.domainperson.model;
     exports com.jarqprog.domainperson.model.user;
 
